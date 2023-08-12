4b27553018dac35952582998b58dfde0e343cbb9
trsaints <thiago.dev2k3@gmail.com>
2023-08-12 12:31:04 -0300

    Monitor files to be merged

--- merge_pdf_files.py
+++ merge_pdf_files.py
@@ -1,4 +1,5 @@
 from os import walk
+from string import Template
 import fitz
 
 def merge_pdfs():
@@ -16,6 +17,7 @@ def merge_pdfs():
 			pdfs.append(current_file)
 
 	if len(pdfs) == 0:
+		print("There are no PDF files to process in the current directory. Operation finished")
 		return
    
 	pdfs.sort(key=lambda x: x.replace(".pdf", ""))
@@ -27,20 +29,19 @@ def merge_pdfs():
 		current_index = pdfs.index(current_pdf)
 		next_index = current_index + 1
     
-		if next_index >= len(pdfs):
+		if next_index >= len(pdfs): 
+			print(Template("Merging the following files: ${files}").substitute(files = current_file_group))
 			break
 
 		next_pdf = pdfs[next_index]
 
 		if next_pdf.startswith(pdf_name):
 			current_file_group.append(next_pdf)
-		else:
+		else:	
+			print(Template("Merging the following files: ${files}").substitute(files = current_file_group))
 			current_file_group = []
 			current_file_group.append(next_pdf)
-			pdf_name = next_pdf.replace(".pdf", "")
-	
-		print(current_file_group)
 
-	print(pdfs)
+			pdf_name = next_pdf.replace(".pdf", "")
 
 merge_pdfs()
