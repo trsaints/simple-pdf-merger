de72e15a2eccd55bdaaaf3c71228f0e1df1a072a
trsaints <thiago.dev2k3@gmail.com>
2023-08-13 10:44:56 -0300

    Create automatic installation script

---
+++ install.ps1
@@ -0,0 +1,9 @@
+Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
+$script = New-Object Net.WebClient
+$script.DownloadString("https://chocolatey.org/install.ps1")
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+. $PROFILE
+choco upgrade chocolatey
+choco install -y --force python3
+py -m pip install --upgrade pip
+py -m pip install pymupdf
