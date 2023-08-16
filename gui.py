1d19c1d61b43e74959a6e2ad5814d40ff5047a4f
trsaints <thiago.dev2k3@gmail.com>
2023-08-16 20:53:51 -0300

    Adjust formatting

--- gui.py
+++ gui.py
@@ -6,33 +6,33 @@ from tkinter.messagebox import showwarning, showerror, showinfo
 from string import Template
 
 class Application(tk.Frame):
-	def __init__(self, master=None):
+	def __init__(self, master = None):
 		tk.Frame.__init__(
 			self, 
 			master,
-			bg="#e2dfe0",
-			padx=12,
-			pady=6
+			bg = "#e2dfe0",
+			padx = 12,
+			pady = 6
 		)
 
 		self.main_font = tk_font.Font(
-			family='Segoe UI', 
-			size=12, 
-			weight='normal'
+			family = "Segoe UI", 
+			size = 12, 
+			weight = "normal"
 		)
 		self.main_font_bold = tk_font.Font(
-			family="Segoe UI",
-			size=12,
-			weight="bold"
+			family = "Segoe UI",
+			size = 12,
+			weight = "bold"
 		)
 		self.mono_font = tk_font.Font(
-			family='Cascadia Mono',
-			size=9,
-			weight='normal'
+			family = "Cascadia Mono",
+			size = 9,
+			weight = "normal"
 		)
 
 		self.grid(
-			sticky=tk.N+tk.E+tk.S+tk.W
+			sticky = tk.N+tk.E+tk.S+tk.W
 		)
 		self.create_widgets()
 		self.render_widgets()
@@ -49,110 +49,110 @@ class Application(tk.Frame):
 	def create_widgets(self):
 		self.origin_label_frame = tk.LabelFrame(
 			self,
-			labelanchor="nw",
-			font=self.main_font_bold,
-			text="Pasta de origem",
-			bg="#e2dfe0"
+			labelanchor = "nw",
+			font = self.main_font_bold,
+			text = "Pasta de origem",
+			bg = "#e2dfe0"
 		)
 		self.destination_label_frame = tk.LabelFrame(
 			self,
-			labelanchor="nw",
-			font=self.main_font_bold,
-			text="Pasta de destino",
-			bg="#e2dfe0"
+			labelanchor = "nw",
+			font = self.main_font_bold,
+			text = "Pasta de destino",
+			bg = "#e2dfe0"
 		)
 		self.origin_button = tk.Button(
 			self.origin_label_frame, 
-			text='Selecionar pasta', 
-			font=self.main_font, 
-			command=self.set_origin,
-			bg="#bcb3b3"
+			text = "Selecionar pasta", 
+			font = self.main_font, 
+			command = self.set_origin,
+			bg = "#bcb3b3"
 		)
 		self.destination_button = tk.Button(
 			self.destination_label_frame, 
-			text='Selecionar pasta', 
-			font=self.main_font, 
-			command=self.set_destination,
-			bg="#bcb3b3"
+			text = "Selecionar pasta", 
+			font = self.main_font, 
+			command = self.set_destination,
+			bg = "#bcb3b3"
 		)
 		self.merge_button = tk.Button(
 			self, 
-			text='Unificar PDFs', 
-			font=self.main_font, 
+			text = "Unificar PDFs", 
+			font = self.main_font, 
 			command = self.merge_files,
-			bg="#48bf84",
+			bg = "#48bf84",
 		)
 		self.origin_display = tk.Text(
 			self.origin_label_frame,
-			font=self.mono_font,
-			padx=8,
-			pady=4,
-			width=100,
-			height=1,
-			state="disabled"
+			font = self.mono_font,
+			padx = 8,
+			pady = 4,
+			width = 100,
+			height = 1,
+			state = "disabled"
 		)
 		self.destination_display = tk.Text(
 			self.destination_label_frame,
-			font=self.mono_font,
-			padx=8,
-			pady=4,
-			width=100,
-			height=1,
-			state="disabled"
+			font = self.mono_font,
+			padx = 8,
+			pady = 4,
+			width = 100,
+			height = 1,
+			state = "disabled"
 		)
 		
 	def render_widgets(self):	
 		self.origin_button.grid(
-			row=0, 
-			column=2, 
-			columnspan=3, 
-			padx=12, 
-			pady=8, 
-			ipadx=8,
-			sticky=tk.E+tk.W
+			row = 0, 
+			column = 2, 
+			columnspan = 3, 
+			padx = 12, 
+			pady = 8, 
+			ipadx = 8,
+			sticky = tk.E+tk.W
 		)
 		self.destination_button.grid(
-			row=1, 
-			column=2, 
-			columnspan=3, 
-			padx=12, 
-			pady=8, 
-			ipadx=8,
-			sticky=tk.E+tk.W
+			row = 1, 
+			column = 2, 
+			columnspan = 3, 
+			padx = 12, 
+			pady = 8, 
+			ipadx = 8,
+			sticky = tk.E+tk.W
 		)
 		self.merge_button.grid(
-			row=2, 
-			column=0, 
-			columnspan=3, 
-			padx=12, 
-			pady=8, 
-			sticky=tk.E+tk.W
+			row = 2, 
+			column = 0, 
+			columnspan = 3, 
+			padx = 12, 
+			pady = 8, 
+			sticky = tk.E+tk.W
 		)
 		self.origin_display.grid(
-			row=0,
-			column=1,
-			padx=12,
-			pady=8,
+			row = 0,
+			column = 1,
+			padx = 12,
+			pady = 8,
 		)
 		self.destination_display.grid(
-			row=1,
-			column=1,
-			padx=12,
-			pady=8,
+			row = 1,
+			column = 1,
+			padx = 12,
+			pady = 8,
 		)
 		self.origin_label_frame.grid(
-			row=0,
-			column=1,
-			padx=12,
-			pady=8,
-			sticky=tk.NW
+			row = 0,
+			column = 1,
+			padx = 12,
+			pady = 8,
+			sticky = tk.NW
 		)	
 		self.destination_label_frame.grid(
-			row=1,
-			column=1,
-			padx=12,
-			pady=8,
-			sticky=tk.NW
+			row = 1,
+			column = 1,
+			padx = 12,
+			pady = 8,
+			sticky = tk.NW
 		)
 
 	def get_path_message(self, target):
@@ -199,13 +199,13 @@ class Application(tk.Frame):
 	def render_selection_error(self, target):		
 		if target == "origin":
 			showerror(
-				title="Falha ao unificar arquivos",
-				message="Nenhuma pasta de origem foi selecionada"
+				title = "Falha ao unificar arquivos",
+				message = "Nenhuma pasta de origem foi selecionada"
 			)
 		elif target == "destination":
 			showerror(
-				title="Falha ao unificar arquivos",
-				message="Nenhuma pasta de destino foi selecionada"
+				title = "Falha ao unificar arquivos",
+				message = "Nenhuma pasta de destino foi selecionada"
 			)
 	
 	def check_selection(self):
@@ -228,13 +228,15 @@ class Application(tk.Frame):
 		if selection_status == False: return
 
 		file_merger.initialize(
-			origin=self.origin_path, 
-			destination=self.destination_path
+			origin = self.origin_path, 
+			destination = self.destination_path
 		)		
 
-		info_message = Template("Seus arquivos foram unificados com sucesso. Você pode encontrá-los em ${destination}").substitute(destination = self.destination_path)
+		info_message = Template(
+				"Seus arquivos foram unificados com sucesso. Você pode encontrá-los em ${destination}"
+			).substitute(destination = self.destination_path)
 
 		showinfo(
-			title="Operação concluída.",
-			message=info_message
-        )
+			title = "Operação concluída.",
+			message = info_message
+    )
