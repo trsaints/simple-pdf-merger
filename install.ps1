4f5f3e764d926f8cca77855d1288dbebad56c63e
trsaints <thiago.dev2k3@gmail.com>
2023-08-18 10:53:31 -0300

    Adjust app installation script

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 py -m pip install --upgrade pip
 py -m pip install pymupdf
-py -m py_compile gui.py
-py -m py_compile merge_pdf_files.py
+py -m py_compile ./simple_pdf_merger/application_gui.py
+py -m py_compile ./simple_pdf_merger/file_merger.py
