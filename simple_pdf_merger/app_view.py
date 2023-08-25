028e2421768f7ed9331dbbf0939b19275374a71f
trsaints <thiago.dev2k3@gmail.com>
2023-08-24 21:57:37 -0300

    Separate rendering responsibility - II

---
+++ simple_pdf_merger/app_view.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+
+def render_gui(gui):
+	gui.origin_button.grid(
+		row = 0,
+		column = 2,
+		columnspan = 3,
+		padx = 12,
+		pady = 8,
+		ipadx = 8,
+		sticky = tk.E+tk.W
+	)
+	gui.destination_button.grid(
+		row = 1,
+		column = 2,
+		columnspan = 3,
+		padx = 12,
+		pady = 8,
+		ipadx = 8,
+		sticky = tk.E+tk.W
+	)
+	gui.merge_button.grid(
+		row = 2,
+		column = 0,
+		columnspan = 3,
+		padx = 12,
+		pady = 8,
+		sticky = tk.E+tk.W
+	)
+	gui.origin_display.grid(
+		row = 0,
+		column = 1,
+		padx = 12,
+		pady = 8
+	)
+	gui.destination_display.grid(
+		row = 1,
+		column = 1,
+		padx = 12,
+		pady = 8
+	)
+	gui.origin_label_frame.grid(
+		row = 0,
+		column = 1,
+		padx = 12,
+		pady = 8,
+		sticky = tk.NW
+	)
+	gui.destination_label_frame.grid(
+		row = 1,
+		column = 1,
+		padx = 12,
+		pady = 8,
+		sticky = tk.NW
+	)
+	gui.grid(sticky = tk.N+tk.E+tk.S+tk.W)
+
+def update_path_output(target, output):
+	target.configure(state = "normal")
+	target.delete("1.00", "end")
+	target.insert("end", output)
